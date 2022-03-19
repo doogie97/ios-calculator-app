bb46e999522e822feaf287ed2ca85a6eba5de51a
doogie97 <chris_97@naver.com>
2022-03-19 13:41:32 +0900

    feat: componentsByOperators 함수 구현

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -2,7 +2,15 @@ import Foundation
 
 enum ExpressionParser {
     func parse(from input: String) -> Formula {
-    }
+    } // 여기서 반환된 Formula가 testcase에서 sut에 넣으면 될듯? 암아두
+    
     private func componentsByOperators(from input: String) -> [String] {
+        var resultArray: [String] = [input]
+        for operation in Operator.allCases {
+            if input.contains(operation.rawValue) {
+                resultArray = input.split(with: operation.rawValue)
+            }
+        }
+        return resultArray
     }
 }
---
+++ Calculator/ExpressionParserTest/ExpressionParserTest.swift
@@ -0,0 +1,17 @@
+//
+//  ExpressionParserTest.swift
+//  ExpressionParserTest
+//
+//  Created by 최최성균 on 2022/03/19.
+//
+
+import XCTest
+@testable import Calculator
+class ExpressionParserTest: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
