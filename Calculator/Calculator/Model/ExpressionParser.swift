02faf54c793708ae49b65e550da7279036c8fd57
doogie97 <chris_97@naver.com>
2022-03-19 12:32:06 +0900

    feat: ExpressionParser 열거형 생성, refactor: split함수 수정

---
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+enum ExpressionParser {
+    func parse(from input: String) -> Formula {
+    }
+    private func componentsByOperators(from input: String) -> [String] {
+    }
+}
--- Calculator/Calculator/Model/Extension.swift
+++ Calculator/Calculator/Model/Extension.swift
@@ -5,7 +5,9 @@ extension Double: CalculateItem {}
 extension String {
     func split(with target: Character) -> [String] {
         var result = self.split(separator: target).compactMap({String($0)})
-        result.append(String(target))
+        if self.contains(target) {
+            result.append(String(target))
+        }
         return result
     }
 }
